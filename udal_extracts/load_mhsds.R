e3a0cea94a291d4a210641782cef36c3eff4a7f8
ASW-Analyst <andy.wilson8@nhs.net>
2024-10-15 19:12:37 +0100

    Load baseline data outputs

---
+++ udal_extracts/load_mhsds.R
@@ -0,0 +1,21 @@
+
+# Load Packages -----------------------------------------------------------
+
+library(here)
+source(paste0(here(), "/udal_extracts/packages.R"))
+
+
+# UDAL Connection ---------------------------------------------------------
+
+source(paste0(here(), "/udal_extracts/udal_connect.R"))
+
+# Run Extract Script ------------------------------------------------------
+
+DBI::dbExecute(con, statement = read_file(paste0(here(),"/udal_extracts/mhsds_baseline.sql")), immediate=TRUE)
+mhsds_baseline_adm <- DBI::dbGetQuery(con, statement = read_file(paste0(here(),"/udal_extracts/read_admissions_output.sql")))
+mhsds_baseline_ws <- DBI::dbGetQuery(con, statement = read_file(paste0(here(),"/udal_extracts/read_wardstays_output.sql")))
+
+
+# Drop Temps --------------------------------------------------------------
+
+DBI::dbExecute(con, statement = read_file(paste0(here(),"/udal_extracts/drop_temps.sql")), immediate=TRUE)
