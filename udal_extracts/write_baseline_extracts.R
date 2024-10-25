79428cc6bdeab8b00d1138c640d37b1b30528e08
ASW-Analyst <andy.wilson8@nhs.net>
2024-10-25 13:36:08 +0100

    Add write to csv extract script

---
+++ udal_extracts/write_baseline_extracts.R
@@ -0,0 +1,2 @@
+write.csv(mhsds_baseline_adm, file = paste0(here(), "/udal_extracts/data/mhsds_baseline_adm.csv"))
+write.csv(mhsds_baseline_ws, file = paste0(here(), "/udal_extracts/data/mhsds_baseline_ws.csv"))
