005e58c53d784cc3ffbe2e26e08f4fba81ca2762
Alexander Lawless <alexander.lawless1@nhs.net>
2026-09-23 13:04:52 +0100

    Added land-based projections to deploy script

--- deploy.R
+++ deploy.R
@@ -5,6 +5,7 @@ rsconnect::deployApp(
     # Add any additional files unique to your app here.
     "app.R",
     "demographic_projections/icb_weighted_demographic_change.csv",
+    "demographic_projections/icb_population_projection_land_based.csv",
     "www/styles.css",
     "www/tsu_logo_black.png",
     "www/baseline_aggregate_pseudo_icb.csv",
